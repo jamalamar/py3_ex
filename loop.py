7201dc9e1537615308dc1ce2349dd06ccb7e0387
jamalamar <jamal_ab22@hotmail.com>
2019-07-21 17:15:15 -0500

    simple for loop

--- loop.py
+++ loop.py
@@ -8,4 +8,12 @@ while i <= 100:
  	i += 1
 
 #Once loop is finished print this:
-print("Done with loop")
+print("Done with While-Loop")
+
+
+
+#Simple For-Loop
+for letter in "This is a String":
+	print(letter)
+
+print("Done with For-Loop")
