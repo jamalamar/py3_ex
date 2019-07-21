c5a141af44319724f29490e8579af7c1f48f088a
jamalamar <jamal_ab22@hotmail.com>
2019-07-21 15:50:24 -0500

    modified game

--- guessing_game.py
+++ guessing_game.py
@@ -1,8 +1,16 @@
 
 secret_word = "giraffe"
 guess = ""
+tries = 3
 
-while guess != secret_word:
+
+while guess != secret_word and tries > 0:
 	guess = input("Enter guess: ")
+	
+	tries -= 1
+	
+	if tries == 0:
+		print("Game Over")
 
-print("You Win!")
+	elif guess == secret_word:
+		print("You Win!")
