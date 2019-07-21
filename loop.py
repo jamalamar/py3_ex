ee15cf7f94de69f1437d2e83f65fad17af4e79fb
jamalamar <jamal_ab22@hotmail.com>
2019-07-21 17:22:06 -0500

    new comments and new loops

--- loop.py
+++ loop.py
@@ -10,10 +10,38 @@ while i <= 100:
 #Once loop is finished print this:
 print("Done with While-Loop")
 
-
+#########################################
 
 #Simple For-Loop
 for letter in "This is a String":
 	print(letter)
 
 print("Done with For-Loop")
+
+
+#########################################
+
+#For-Loop with Arrays
+friends = ["Jim", "Karen", "Kevin"]
+for friend in friends:
+	print(friend)
+
+#Same result
+for index in range(len(friends)):
+	print(friends[index])
+
+#########################################
+
+
+#For-Loop with numbers
+for index in range(25):
+	print(index)
+
+for index in range(5, 10):
+	print(index)
+
+
+#########################################
+
+
+
